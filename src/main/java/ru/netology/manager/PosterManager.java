ab5bfa2bfced47161a4c9f82455d5d2f941d921c
Deportator <fabrichnov87@gmail.com>
2022-06-14 21:53:42 +0300

    Fixed2

--- src/main/java/ru/netology/manager/PosterManager.java
+++ src/main/java/ru/netology/manager/PosterManager.java
@@ -5,13 +5,15 @@ import ru.netology.domain.PosterItem;
 public class PosterManager {
 
     private PosterItem[] items = new PosterItem[0];
-    private int amountPoster = 10;
-    public PosterManager(int amountPoster) {
-        this.amountPoster = amountPoster;
+    private int defaultAmountPoster = 10;
+    public PosterManager(int defaultAmountPoster) {
+        this.defaultAmountPoster = defaultAmountPoster;
     }
 
     public PosterManager() {
     }
+
+
     public void add(PosterItem item) {
         int lenght = items.length + 1;
         PosterItem[] tmp = new PosterItem[lenght];
@@ -22,20 +24,34 @@ public class PosterManager {
     }
 
     public PosterItem[] findAll() {return items;}
-
-    public PosterItem[] getLastAdd() {
-//        int resultLength = 10;
 //
-//        if (items.length < 10) {
-//            resultLength = items.length;
-//        }
+    public PosterItem[] getLastAdd() {
+        int resultLength = 7;
 
-        PosterItem[] result = new PosterItem[amountPoster];
-        for (int i = 0; i < amountPoster; i++) {
+        if (items.length < 7) {
+            resultLength = items.length;
+        }
+//        PosterItem[] items = new PosterItem[]{};
+
+        PosterItem[] result = new PosterItem[items.length];
+        for (int i = 0; i < result.length; i++) {
             int index = items.length - i - 1;
             result[i] = items[index];
         }
         return result;
     }
 
-   }
+//    public PosterItem[] getLastAdd() {
+//        int resultLength = items.length;
+//        if (items.length < defaultAmountPoster) {
+//            defaultAmountPoster = resultLength;
+//        }
+//        PosterItem[] result = new PosterItem[][defaultAmountPoster];
+//        for (int i = 0; i < defaultAmountPoster; i++) {
+//            int index = resultLength - i - 1;
+//            result [i] = items[index];
+//
+//        }
+//        return result;
+
+}
--- src/test/java/ru/netology/manager/PosterManagerTest.java
+++ src/test/java/ru/netology/manager/PosterManagerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import ru.netology.domain.PosterItem;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 public class PosterManagerTest {
     private PosterItem first = new PosterItem(1, "Bladshot");
     private PosterItem second = new PosterItem(2, "Ahead");
@@ -20,11 +21,143 @@ public class PosterManagerTest {
     private PosterItem ninth = new PosterItem(9, "NumberThree");
     private PosterItem tenth = new PosterItem(10, "NumberFour");
 
-    PosterItem[] expected = {tenth, ninth, eighth, seventh, sixth, fifth, fourth, third, second, first};
+//    PosterItem[] expected = {tenth, ninth, eighth, seventh, sixth, fifth, fourth, third, second, first};
+//  PosterItem[] expected = {first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth};
+
+//    @Test
+//    void shouldShowLessLimit() {
+//        PosterManager manager = new PosterManager(5);
+//        manager.getLastAdd(first);
+//        manager.getLastAdd(second);
+//        manager.getLastAdd(third);
+//        manager.getLastAdd(fourth);
+//        manager.getLastAdd(fifth);
+//        PosterItem[] actual = manager.getLastAdd();
+//        assertArrayEquals(expected, actual);
+//
+//    }
+
+//    @Test
+//    void mustShowTenPoster() {
+//        PosterManager manager = new PosterManager(10);
+//        manager.add(first);
+//        manager.add(second);
+//        manager.add(third);
+//        manager.add(fourth);
+//        manager.add(fifth);
+//        manager.add(sixth);
+//        manager.add(seventh);
+//        manager.add(eighth);
+//        manager.add(ninth);
+//        manager.add(tenth);
+//        PosterItem[] actual = manager.getLastAdd();
+//        assertArrayEquals(expected, actual);
+//    }
+//
+//    @Test
+//    void mustShowTen() {
+//        PosterManager manager = new PosterManager(10);
+//        manager.add(first);
+//        manager.add(second);
+//        manager.add(third);
+//        manager.add(fourth);
+//        manager.add(fifth);
+//        manager.add(sixth);
+//        manager.add(seventh);
+//        manager.add(eighth);
+//        manager.add(ninth);
+//        manager.add(tenth);
+//        PosterItem[] actual = manager.getLastAdd();
+//        assertArrayEquals(expected, actual);
+//    }
+
+//    @Test
+//    void mustShowValid() {
+//        PosterManager manager = new PosterManager(5);
+//        manager.add(first);
+//        manager.add(second);
+//        manager.add(third);
+//        manager.add(fourth);
+//        manager.add(fifth);
+//        manager.add(sixth);
+//        manager.add(seventh);
+//        manager.add(eighth);
+//        manager.add(ninth);
+//        manager.add(tenth);
+//        PosterItem itemAdd = new PosterItem(11, "NumberFive");
+//        manager.add(itemAdd);
+//        PosterItem[] actual = manager.getLastAdd();
+//        PosterItem[] expected = {new PosterItem(11, "NumberFive"), tenth, ninth, eighth, seventh};
+//        assertArrayEquals(expected, actual);
+//
+//    }
+
+//    @Test
+//    void mustShowEquallyLimit() {
+//        PosterManager manager = new PosterManager(10);
+//        manager.add(first);
+//        manager.add(second);
+//        manager.add(third);
+//        manager.add(fourth);
+//        manager.add(fifth);
+//        manager.add(sixth);
+//        manager.add(seventh);
+//        manager.add(eighth);
+//        manager.add(ninth);
+//        manager.add(tenth);
+//        PosterItem[] actual = manager.getLastAdd();
+//        assertArrayEquals(expected, actual);
+//
+//    }
+//
+//    @Test
+//    void mustShowLessLimit() {
+//        PosterManager manager = new PosterManager(10);
+//        manager.add(first);
+//        manager.add(second);
+//        manager.add(third);
+//        manager.add(fourth);
+//        manager.add(fifth);
+//
+//        PosterItem[] actual = manager.getLastAdd();
+//        assertArrayEquals(expected, actual);
+//
+//    }
 
     @Test
-    void mustShowTenPoster() {
-        PosterManager manager = new PosterManager(10);
+    void mustShowAddPoster() {
+        PosterManager manager = new PosterManager();
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+
+        PosterItem[] actual = manager.findAll();
+        PosterItem[] expected = new PosterItem[]{first, second, third, fourth, fifth};
+        assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    void mustShowLessLimit() {
+        PosterManager manager = new PosterManager();
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+
+        manager.getLastAdd();
+
+        PosterItem[] actual = manager.findAll();
+        PosterItem[] expected = new PosterItem[]{fifth, fourth, third, second, first};
+        assertArrayEquals(expected, actual);
+
+    }
+    @Test
+    void mustShowEquallyLimit() {
+        PosterManager manager = new PosterManager();
         manager.add(first);
         manager.add(second);
         manager.add(third);
@@ -32,16 +165,19 @@ public class PosterManagerTest {
         manager.add(fifth);
         manager.add(sixth);
         manager.add(seventh);
-        manager.add(eighth);
-        manager.add(ninth);
-        manager.add(tenth);
-        PosterItem[] actual = manager.getLastAdd();
+
+
+
+        manager.getLastAdd();
+
+        PosterItem[] actual = manager.findAll();
+        PosterItem[] expected = new PosterItem[]{seventh, sixth, fifth, fourth, third, second, first};
         assertArrayEquals(expected, actual);
     }
 
     @Test
-    void mustShowTen() {
-        PosterManager manager = new PosterManager(10);
+    void mustShowOverLimit() {
+        PosterManager manager = new PosterManager();
         manager.add(first);
         manager.add(second);
         manager.add(third);
@@ -50,49 +186,13 @@ public class PosterManagerTest {
         manager.add(sixth);
         manager.add(seventh);
         manager.add(eighth);
-        manager.add(ninth);
-        manager.add(tenth);
-        PosterItem[] actual = manager.getLastAdd();
+
+
+        manager.getLastAdd();
+
+        PosterItem[] actual = manager.findAll();
+        PosterItem[] expected = new PosterItem[]{eighth, seventh, sixth, fifth, fourth, third, second, first};
         assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    void mustShowValid() {
-        PosterManager manager = new PosterManager(5);
-        manager.add(first);
-        manager.add(second);
-        manager.add(third);
-        manager.add(fourth);
-        manager.add(fifth);
-        manager.add(sixth);
-        manager.add(seventh);
-        manager.add(eighth);
-        manager.add(ninth);
-        manager.add(tenth);
-        PosterItem itemAdd = new PosterItem(11, "NumberFive");
-        manager.add(itemAdd);
-        PosterItem[] actual = manager.getLastAdd();
-        PosterItem[] expected = {new PosterItem(11, "NumberFive"), tenth, ninth, eighth, seventh};
-        assertArrayEquals(expected, actual);
-
-    }
-
-    @Test
-    void mustShowOverMax() {
-        PosterManager manager = new PosterManager(10);
-        manager.add(first);
-        manager.add(second);
-        manager.add(third);
-        manager.add(fourth);
-        manager.add(fifth);
-        manager.add(sixth);
-        manager.add(seventh);
-        manager.add(eighth);
-        manager.add(ninth);
-        manager.add(tenth);
-        PosterItem[] actual = manager.getLastAdd();
-        assertArrayEquals(expected, actual);
-
     }
 
 
